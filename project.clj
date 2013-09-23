01409ab194e747a9ed47f4817a66f8ae8bbf01ec
robertjlooby <robertjlooby@gmail.com>
2013-09-23 16:33:53 -0500

    changed version and added clojurescript dep

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "JavaTicTacToe in browser with clojure_server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.codec "0.1.0"]
                  [org.clojure/data.json  "0.2.3"]
                  [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojure/clojurescript"0.0-1889"]
                  [org.clojure/google-closure-library-third-party "0.0-2029"]
                  [domina "1.0.0"]]
   :profiles {:dev {:dependencies [[speclj "2.5.0"]
