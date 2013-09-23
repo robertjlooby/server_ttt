136590fded6a06963e52858482bb86f4537744e7
robertjlooby <robertjlooby@gmail.com>
2013-09-23 16:27:13 -0500

    stripped out all the coffeescript/javascript

--- project.clj
+++ project.clj
@@ -5,14 +5,24 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.codec "0.1.0"]
                  [org.clojure/data.json  "0.2.3"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojure/google-closure-library-third-party "0.0-2029"]
+                 [domina "1.0.0"]]
   :profiles {:dev {:dependencies [[speclj "2.5.0"]
                                   [junit/junit "4.10"]]}}
   :plugins [[speclj "2.5.0"]
-            [lein-junit "1.1.2"]]
+            [lein-junit "1.1.2"]
+            [lein-cljsbuild "0.3.3"]]
   :jvm-opts ["-Dline.separator=\r\n"]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java" "spec/java"]
   :junit ["spec/java"]
   :test-paths ["spec/clojure"]
+  :cljsbuild {
+              :builds [{
+                        :source-paths ["src/clojurescript"]
+                        :compiler {
+                                   :output-to "ttt_public/javascripts/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]}
   :main clojure_server.ttt-server)
